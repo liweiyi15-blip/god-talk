3310da726ebc5a4be38b31e2d7b36b87a59dec79
god <liweiyi15@gmail.com>
2026-05-14 14:14:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class TradeShare(commands.Cog):
 
         # 设置 Embed 颜色为 #8F4313 (十六进制 0x8F4313)
         embed = discord.Embed(
-            title="仙人说：", 
+            title="仙人说了：", 
             description=content,
             color=discord.Color(0x8F4313)
         )
